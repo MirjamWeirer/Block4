55366fc20c60069da2437ad68cd9623717d98f8e
MirjamWeirer <mirjam.weirer@gmx.at>
2021-03-15 09:17:07 +0100

    Initial commit

---
+++ src/S2.java
@@ -0,0 +1,13 @@
+public class S2 {
+    public static void main(String[] args) {
+        for (int i = 100; i >= 1; i--) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+        int i = 100;
+        while (i >= 1){
+            System.out.print(i + "; ");
+            i--;
+        }
+    }
+}
