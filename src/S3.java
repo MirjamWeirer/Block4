8ac4f57b8e5438e2d5732ab57da89c49015506be
MirjamWeirer <mirjam.weirer@gmx.at>
2021-03-15 11:45:58 +0100

    übung s3

---
+++ src/S3.java
@@ -0,0 +1,10 @@
+public class S3 {
+    public static void main(String[] args) {
+        //Zahlen zwischen 1 und 100 addieren
+        int sum = 0;
+        for (int i = 1; i <= 100 ; i++) {
+            sum += i;
+        }
+        System.out.println("sum = " + sum);
+    }
+}
