07d03734e994b906cedde85f64ce5d944056a1fd
MirjamWeirer <mirjam.weirer@gmx.at>
2021-03-18 14:30:33 +0100

    Narzisstische Zahlen

--- src/NarzisstischeZahlenDemo.java
+++ src/NarzisstischeZahlenDemo.java
@@ -1,24 +1,28 @@
 public class NarzisstischeZahlenDemo {
     public static void main(String[] args) {
-        //Eine Narzisstische Zahl ergibt sich aus der Potenz die gleich der Anzahl ihrer Ziffern ist z.B. 153= 1³ + 5³ +³
+        //Eine Narzisstische Zahl ergibt sich aus der Potenz die gleich der Anzahl ihrer Ziffern ist z.B. 153= 1³ + 5³ +3³
         //alle Narzisstischen Zahl zwischen 1 und 1.000 ausgeben
-        narzisstischeZahl();
-    }
-    public static void narzisstischeZahl(){
-        int ziffer = 0;
-        for (int i = 1; i < 1000; i++) {
-            int temp = 1;
-            int zahl = 0;
 
-            while (temp !=0){
-                ziffer = temp % 10;
-                zahl += (ziffer * ziffer * ziffer);
-                temp /= 10;
-            }
 
-            if (zahl == i){
-                System.out.print(zahl + ", ");
+        for (int exp = 3, exp <= 8; exp++){
+            for (int i = 0; i < 30_000_000; i++) {
+                if (narzisstischeZahl(i, exp)){
+                    System.out.println(i + ", ");
+                }
             }
         }
+    }
+    public static boolean narzisstischeZahl(int number, int exp){
+       int sum = 0;
+       int originalValue = number;
+       while (number > 0){
+           int digit = number % 10;
+           number = number / 10;
+           sum = sum + (int)Math.pow(digit, exp);
+       }
+       if (sum == originalValue){
+           return true;
+       }
+       return false;
     }
 }
--- src/VollkommeneZahlenDemo.java
+++ src/VollkommeneZahlenDemo.java
@@ -1,25 +1,29 @@
 public class VollkommeneZahlenDemo {
     /*
         Vollkommene Zahlen sind die Summe ihrer echten Teiler z.B. 6=1+2+3
-        Aufgabe einen Algorithums schreiben, der alle Vollkommenenzahlen bis 100000000 ausfibt
+        Aufgabe einen Algorithums schreiben, der alle Vollkommenenzahlen bis 100000000 ausgibt
      */
     public static void main(String[] args) {
-        calcNumber();
-    }
-    public static void calcNumber(){
-        for (long i = 1; i <= 100000000; i++) {
-            int sum = 0;
-            for (long j = 1; j < i; j++){
-                if (i % j == 0){
-                    sum += j;
-                }
-                if (sum > i){
-                    break;
-                }
-            }
-            if (sum == i){
-                System.out.println(sum + " Vollkommene Zahl");
+        for (int i = 0; i < 100_000_000; i++) {
+            if (isPerfectNumber(i) == true){
+                System.out.println(i + " is a perfect number");
             }
         }
+    }
+    public static int calcNumber (int number){
+      int sum = 0;
+        for (int i = 1; i <= number / 2 ; i++) {
+            if (number % i == 0) {
+                sum = sum + i;
+            }
+        }
+      return sum;
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        if (number == calcNumber(number)){
+            return true;
+        }
+        return false;
     }
 }
